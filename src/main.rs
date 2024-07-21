3c209b74f815952a7d8c58d05803db236cd199c8
Alex Wood <aeshtaer@gmail.com>
2024-07-21 07:28:00 -0400

    Print parse result
    It's a RPL (read-print-loop)!

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,41 @@ use pest_derive::Parser;
 #[grammar = "sexp.pest"]
 pub struct SexpParser;
 
+fn print_parse(parse: pest::iterators::Pair<Rule>) {
+    match parse.as_rule() {
+        Rule::symbol => { print!("{}", parse.as_str()); }
+        Rule::integer => { print!("{}", parse.as_str()); }
+        Rule::boolean => { print!("{}", parse.as_str()); }
+        Rule::proper_list => {
+            print!("(");
+            let mut inner = parse.into_inner();
+            match inner.next() {
+                None => {}
+                Some(p) => {
+                    print_parse(p);
+                    for i in inner {
+                        print!(" ");
+                        print_parse(i);
+                    }
+                }
+            };
+            print!(")");
+        }
+        Rule::dotted_list => {
+            print!("(");
+            let mut inner = parse.into_inner();
+            print_parse(inner.next().unwrap());
+            for i in inner {
+                print!(" ");
+                print_parse(i);
+            }
+            print!(")");
+        }
+        Rule::consing_dot => { print!("."); } // KLUDGE
+        _unknown_term => { panic!("WTF"); }
+    };
+}
+
 fn main() {
     loop {
         print!("> ");
@@ -16,6 +51,12 @@ fn main() {
             .read_line(&mut read)
             .expect("Failed to read line");
         let parse = SexpParser::parse(Rule::sexp, read.as_str());
-        println!("{:?}", parse);
+        match parse {
+            Ok(mut p) => {
+                print_parse(p.next().unwrap());
+                println!("");
+            }
+            Err(e) => { println!("Failed to parse: {:?}", e); }
+        };
     }
 }
